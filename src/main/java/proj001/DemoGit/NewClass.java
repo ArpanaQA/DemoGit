069813b3243ca35f2c7e4aeedcd2931c14300adb
Arpana <muriki.arpana@gmail.com>
2022-06-10 21:54:59 +0530

    First Commit

---
+++ src/main/java/proj001/DemoGit/NewClass.java
@@ -0,0 +1,10 @@
+package proj001.DemoGit;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Welcome to Git and Github");
+	}
+
+}
