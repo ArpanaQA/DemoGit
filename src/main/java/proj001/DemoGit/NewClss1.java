030017c6d4d7249c4ec863c69cac3f49a419d545
Arpana <muriki.arpana@gmail.com>
2022-06-10 22:01:24 +0530

    Second Commit

---
+++ src/main/java/proj001/DemoGit/NewClss1.java
@@ -0,0 +1,10 @@
+package proj001.DemoGit;
+
+public class NewClss1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Demo on Github");
+	}
+
+}
